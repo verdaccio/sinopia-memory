2e07e17eae03f838e66646c7c6e25542db4c8aec
David Gaya <david.gaya@starbreeze.com>
2016-03-01 18:21:58 +0000

    Convert to redis

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 'use strict';
+var redis = require('redis');
 
 module.exports = Memory;
 
 function Memory(config, stuff) {
     var self = Object.create(Memory.prototype);
-    self._users = {};
     self._config = config;
     self._logger = stuff.logger;
     self._sinopia_config = stuff.config;
-
+    self.client = redis.createClient();
     return self
 }
 
 Memory.prototype.authenticate = function (user, password, done) {
-    var self = this
+    var self = this;
 
-    if (!self._users[user]) {
-        return done(null, false);
-    }
+    self.client.get(user, function (err, real_password) {
+      if (err) {
+        return done(err, false);
+      }
 
-    if (password !== self._users[user].password) {
-        return done(null, false);
-    }
+      if (!real_password || password !== real_password) {
+        return done(true, false);
+      }
 
-    // authentication succeeded!
-    // return all usergroups this user has access to;
-    return done(null, [user])
+      // authentication succeeded!
+      // return all usergroups this user has access to;
+      return done(null, [user])
+    });
 
 };
 
 Memory.prototype.adduser = function (user, password, done) {
     var self = this;
-    self._users[user] = {name: user, password: password};
+    self.client.set(user, password, redis.print);
 
     done(null, user);
 };
